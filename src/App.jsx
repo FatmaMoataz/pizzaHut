1e21dc5984c744edcb59827a570d6f243cb289df
FatmaMoataz <fatmamoataz65@gmail.com>
2025-03-21 14:01:21 +0200

    removed lazy from register, login, contact in app

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import { ToastContainer } from 'react-toastify';
 import { lazy, Suspense } from "react";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Contact from "./components/Contact/Contact";
 
 function App() {
 
   const Cart = lazy(() => import('./components/Cart/Cart'))
   const Menu = lazy(() => import('./components/Menu/Menu'))
-  const Contact = lazy(() => import('./components/Contact/Contact'))
   const Favorites = lazy(() => import('./components/Favorites/Favorites'))
-  const Login = lazy(() => import('./components/Login/Login'))
-  const Register = lazy(() => import('./components/Register/Register'))
   const ForgetPassword = lazy(() => import('./components/ForgetPassword/ForgetPassword'))
   const Checkout = lazy(() => import('./components/Checkout/Checkout'))
 
@@ -23,15 +23,15 @@ function App() {
       children: [
         { index: true, element: <Home /> }, 
         { path: "home", element: <Home /> },
-        { path: "menu", element: <Suspense><Menu/> </Suspense>  },
-        { path: "contact", element:  <Suspense><Contact/></Suspense>  },
+        { path: "menu", element: <Suspense fallback={<div>Loading...</div>}><Menu/> </Suspense>  },
+        { path: "contact", element:  <Suspense fallback={<div>Loading...</div>}><Contact/></Suspense>  },
         { path: "about", element:   <About/> },
-        { path: "favorites", element: <Suspense><Favorites/></Suspense>  },
-        { path: "cart", element: <Suspense><Cart/></Suspense>   },
-        { path: "login", element:<Suspense><Login/> </Suspense>  },
-        { path: "register", element: <Suspense><Register/></Suspense>  },
-        { path: "forgetPassword", element: <Suspense> <ForgetPassword/></Suspense>  },
-        { path: "checkout", element: <Suspense><Checkout/></Suspense>  }
+        { path: "favorites", element: <Suspense fallback={<div>Loading...</div>}><Favorites/></Suspense>  },
+        { path: "cart", element: <Suspense fallback={<div>Loading...</div>}><Cart/></Suspense>   },
+        { path: "login", element:<Suspense fallback={<div>Loading...</div>}><Login/> </Suspense>  },
+        { path: "register", element: <Suspense fallback={<div>Loading...</div>}><Register/></Suspense>  },
+        { path: "forgetPassword", element: <Suspense fallback={<div>Loading...</div>}> <ForgetPassword/></Suspense>  },
+        { path: "checkout", element: <Suspense fallback={<div>Loading...</div>}><Checkout/></Suspense>  }
   
       ],
     },
